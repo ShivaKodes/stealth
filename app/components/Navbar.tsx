b144a5b368e980077b21140b21a793e47078b463
Shiva Kumar <shivakodes@gmail.com>
2023-08-18 03:07:35 +0530

    small changes

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,18 +3,21 @@
 import Link from "next/link"
 import Button from "./Button"
 import { Reveal } from "./Reveal"
+import { usePathname, useRouter } from "next/navigation"
+import { twMerge } from "tailwind-merge"
 
 
 const Navbar=()=>{
+    const pathname=usePathname()
     return(
-        <nav className="w-full  ">
+        <nav className="w-full">
             <div className="max-w-5xl mx-auto w-full pt-5 pb-2 flex items-center justify-between  font-medium border-b border-gunmetal/50 px-5 md:px-0">
             <Link href="/" className="text-xl md:text-4xl font-bold text-gunmetal drop-shadow-lg">
                 X
             </Link>
             <Reveal>
 
-            <div className="hidden md:flex items-center gap-x-4 text-base md:text-lg">
+            <div className={twMerge("hidden md:flex items-center gap-x-4 text-base md:text-lg",pathname==='/join-waitlist'&&"md:hidden")}>
                 <Link href={'#Home'}>
                     Home
                 </Link>
@@ -29,9 +32,9 @@ const Navbar=()=>{
                 </Link>
             </div>
             </Reveal>
-            <Button className="bg-gradient-to-r from-gunmetal to-blue-600 text-sm">
+            <Button className={"bg-gradient-to-r from-gunmetal to-blue-600 text-sm"}>
                 <Link href={'/join-waitlist'}>
-                    Join Waitlist
+                    {pathname==='/join-waitlist'?"Let's Go":"Join Waitlist"}
                 </Link>
             </Button>
             </div>
